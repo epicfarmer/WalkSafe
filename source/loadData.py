73fd5999c1a9b9711374539a9151a4c364a8e53c
jkaminsky <magicathering2001@gmail.com>
2016-07-04 23:13:54 -0400

    Started parsing lat/long to numeric

--- source/loadData.py
+++ source/loadData.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-data = pd.read_csv("Data/BPD_Part_1_Victim_Based_Crime_Data.csv")
-latlong_data = data.loc[:,"Location 1"];
+data = pd.read_csv("data/BPD_Part_1_Victim_Based_Crime_Data.csv")
+latlong_data = data.loc[:,"Location 1"].str.split(',')
+print(pd.DataFrame([latlong_data.str[0].str.replace("(",""),latlong_data.str[1].str.replace(")","")]))
+#(latlong_data.str[0].str.replace("(","")
 year = data.loc[:,"CrimeDate"].str.split('/').str.get(2).astype('float')
 
 latlong_data[year>2014]
