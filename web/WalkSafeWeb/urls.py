d0879849a9785f00faf2210deaf64b89b86b1a55
kkintaro <katkintaro@gmail.com>
2016-08-20 22:45:48 -0400

    directionsWithScores returning Google Maps JSON object

--- web/WalkSafeWeb/urls.py
+++ web/WalkSafeWeb/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^BadArea/', include('BadArea.urls')),
+    url(r'^directionsWithScores/', include('directionsWithScores.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
--- web/directionsWithScores/views.py
+++ web/directionsWithScores/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseBadRequest
 import googlemaps
+
 import pprint
 
 DISTANCE_MATRIX_URL ='https://maps.googleapis.com/maps/api/distancematrix/json'
@@ -10,7 +12,7 @@ DIRECTIONS_URL = 'https://maps.googleapis.com/maps/api/directions/json'
 ORIGIN='615 N Wolfe Street, Baltimore, MD'
 DESTINATION = '501 E Pratt Street, Baltimore, MD'
 
-THE_KEY_FILE = ""
+THE_KEY_FILE = "/Users/Kathryn/Desktop/GoogleMapKey"
 BROWSER_KEY = ""
 
 def _api_key():
@@ -31,13 +33,29 @@ def directions(origin, destination):
     API_KEY = _api_key()
     gmaps = googlemaps.Client(key=API_KEY)
     # Request directions via public transit
-    directions_result = gmaps.directions(origin,
+    return gmaps.directions(origin,
                                      destination,
                                      mode="walking")
 
-def index(request):
+def map(request):
+
     html = "<iframe width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\""
     html += "src=\"https://www.google.com/maps/embed/v1/directions?origin=%s&destination=%s&key=%s\"" \
             "allowfullscreen>" \
             "</iframe> " % (ORIGIN, DESTINATION, BROWSER_KEY)
     return HttpResponse(html)
+
+def index(request):
+
+    params = {}
+    if request.method == 'POST':
+        params = request.POST
+    elif request.method == 'GET':
+        params = request.GET
+    else:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
+    if 'origin' not in params.keys() or 'destination' not in params.keys():
+        return HttpResponseBadRequest('<h1>Bad Request</h1>origin and destination parameters required')
+
+    return JsonResponse(directions(params['origin'],params['destination']), safe=False)
