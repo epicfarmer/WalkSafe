7ee098a98961ef2a3e73a1210a9367caeaf50c38
kkintaro <katkintaro@gmail.com>
2016-08-23 17:44:07 -0400

    took out rest_framework

--- web/WalkSafeWeb/settings.py
+++ web/WalkSafeWeb/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,10 +120,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
