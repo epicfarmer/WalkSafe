acd4547f7787c88d1ddff1a07d5fc124a2fb27be
kkintaro <katkintaro@gmail.com>
2016-08-24 22:30:33 -0400

    clean up clean up everybody everywhere

--- web/directionsWithScores/urls.py
+++ web/directionsWithScores/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.directions_with_scores, name='directions_with_scores'),
 ]
